a1054706bbf2ec1a3f645fed55957cd6dc45174b
66010914151 <66010914151@msu.ac.th>
2026-02-10 12:09:51 +0700

    connectdb.php

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,10 +1,11 @@
 <?php
 $host="localhost";
 					$user="root";
-					$pwd="";
+					$pwd="12345678";
 					$db="4151db";
 					$conn=mysqli_connect($host,$user,$pwd,$db) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
 					mysqli_query($conn,"SET NAMES utf8");
 					
 		
+
 					?>
